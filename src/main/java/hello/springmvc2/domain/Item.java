d8d9c0fee878d4052a69b5020e13786feb15750e
BaekEuiHun <euihunbaek@gmail.com>
2025-07-29 21:56:04 +0900

    학습1

---
+++ src/main/java/hello/springmvc2/domain/Item.java
@@ -0,0 +1,23 @@
+package hello.springmvc2.domain;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(Long id, String itemName, Integer price, Integer quantity) {
+        this.id = id;
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Item() {
+    }
+
+
+}
